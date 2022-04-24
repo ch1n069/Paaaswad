1c213d522dce8971b3e2534d0d152d66f6ca3e9e
Moringa School <brunolalpachino@gmail.com>
2022-04-24 14:02:32 +0300

    created a class to store our credintials

---
+++ credentials.py
@@ -0,0 +1,11 @@
+import random 
+
+# this will be used to help generate a custom password for
+
+
+class Credentials():
+
+    user_credentials = []
+
+
+    def __init__(self,platform,user_account_username)
