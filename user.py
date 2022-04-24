8d3771bdaaaead7f5f7ac80d5600b0ea3b4914b5
Moringa School <brunolalpachino@gmail.com>
2022-04-24 13:44:17 +0300

    ccreated instances of the class user

---
+++ user.py
@@ -0,0 +1,8 @@
+class Users:
+
+
+    def __init__(self,firstname,username,password):
+        self.firstname = firstname
+        self.username = username
+        self.password = password
+        
