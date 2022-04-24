c1771e4cab92a7e111fcec9de9837763d6762c9c
Moringa School <brunolalpachino@gmail.com>
2022-04-24 13:49:06 +0300

    defined save and delete functions

--- user.py
+++ user.py
@@ -2,7 +2,24 @@ class Users:
 
     user_list = []
 
+    '''created instance of the class'''
+
     def __init__(self,firstname,username,password):
         self.firstname = firstname
         self.username = username
         self.password = password
+
+    def save_user(self):
+        '''save user to list '''
+
+        Users.user_list.append(self)
+
+    def delete_user(self):
+        '''delete user '''
+
+        Users.user_list.remove(self)
+
+    
+
+
+    
