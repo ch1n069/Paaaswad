91d80172c80aa16f277f957a5d5cdf2bb60fed4a
Moringa School <brunolalpachino@gmail.com>
2022-04-24 13:45:21 +0300

    created dictionary to hold users

--- user.py
+++ user.py
@@ -1,8 +1,8 @@
 class Users:
 
+    user_list = []
 
     def __init__(self,firstname,username,password):
         self.firstname = firstname
         self.username = username
         self.password = password
-        
