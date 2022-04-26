f28556e4927ce4ec5e6002f711d3748f5479e8bc
Moringa School <brunolalpachino@gmail.com>
2022-04-26 08:40:15 +0300

    created test to see user is instantiated correctly

--- user_test.py
+++ user_test.py
@@ -6,4 +6,13 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         "setup to test class"
 
-        self.new_user = Users("bruno")
+        self.new_user = Users("bruno", "chino","1234")
+
+    def test_create_user(self):
+        '''test if user was instantiated correctly'''
+
+        self.assertEqual(self.new_user.firstname, "bruno")
+        self.assertEqual(self.new_user.username, "chino")
+        self.assertEqual(self.new_user.password,"1234")
+
+    def tes
